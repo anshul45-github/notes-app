75da6c0e2db58ff702dca55f5f4c906154801284
Anshul <anshulmendiratta10d@gmail.com>
2025-10-10 16:27:48 +0530

    updated next.config.js to address the issue of missing image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com', // This is the domain for Google profile pics
+          port: '',
+          pathname: '/a/**', // This allows any path starting with /a/
+        },
+      ],
+    },
+  };
 module.exports = nextConfig
